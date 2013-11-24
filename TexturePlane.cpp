56ddf4c91d1abb8e1f616d0b813d27a2bd9a8168
成田 輝久 <hiizoka@gmail.com>
2013-11-24 16:23:35 +0900

    TexturePlaneを追加

---
+++ TexturePlane.cpp
@@ -0,0 +1,22 @@
+//
+//  Plane.cpp
+//  study_texture
+//
+//  Created by 成田 輝久 on 2013/11/24.
+//
+//
+
+#include "TexturePlane.h"
+
+void TexturePlane::setTexturePointer(ofTexture *texture) {
+    if(texture == NULL)return;
+    this->texture = texture;
+    resizeToTexture(*texture);
+}
+
+void TexturePlane::draw() {
+    if(texture != NULL) {
+        texture->bind();
+        ofPlanePrimitive::draw();
+    }
+}
---
+++ TexturePlane.h
@@ -0,0 +1,23 @@
+//
+//  Plane.h
+//  study_texture
+//
+//  Created by 成田 輝久 on 2013/11/24.
+//
+//
+
+#ifndef __study_texture__Plane__
+#define __study_texture__Plane__
+
+#include "ofMain.h"
+
+// Textureを予め割り当てておけば自動でテクスチャを表示するPlane
+class TexturePlane : ofPlanePrimitive {
+public:
+    void setTexturePointer(ofTexture *texture);
+    void draw();
+private:
+    ofTexture *texture;
+};
+
+#endif /* defined(__study_texture__Plane__) */
